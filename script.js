e0b8ab59af491d1b4a23efe422d30be098b3ff31
Pedro López <pedrolopez.e@hotmail.com>
2022-03-02 15:21:52 -0300

    Create function for a single round of the game that returns who wins

--- script.js
+++ script.js
@@ -5,6 +5,46 @@ function computerPlay() {
     return play;
 }
 
-function round() {
-    
+function singleRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLoweCase();
+    let computer = computerSelection.toLowerCase();
+     /* If player has scissors and computer has paper return you win! scissors beats paper*/
+     /* If player has scissors and computer has rock return you lose! Rock beats scissors*/
+     /* If player has scissors and c has scissors return thats a tie*/
+     if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beats paper";
+        } else if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors";
+        } else {
+            return "That's a tie!";
+        }
+    } else if (playerSelection === "rock") {
+        /* If player has rock and computer has paper return you lose! paper beats rock*/
+        /* If player has rock and computer has scissors return you win! Rock beats scissors*/
+        /* If player has rock and c has rock return thats a tie*/
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock";
+        } else if (computerSelection === "rock") {
+            return "That's a tie!";
+        } else {
+            return "You win! Rock beats scissors!";
+        }
+    } else {
+        /* If player has paper and computer has scissors return you lose! scissors beats paper*/
+        /* If player has paper and computer has rock return you win! paper beats rock*/
+        /* If player has paper and c has paper return thats a tie*/
+        if (computerSelection === "paper") {
+            return "That's a tie!";
+        } else if (computerSelection === "rock") {
+            return "You win! Paper beats rock";
+        } else {
+            return "You lose! Scissors beats paper";
+        }
+
+    }
+     
+     
+
+     
 }
