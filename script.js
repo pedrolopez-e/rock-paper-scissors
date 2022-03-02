815c633222174fc5a4cfbcdc2ed7a2675bee2a19
Pedro López <pedrolopez.e@hotmail.com>
2022-03-02 12:25:50 -0300

    Create computerPlay function

---
+++ script.js
@@ -0,0 +1,10 @@
+/*Function that simulates the computer play*/
+function computerPlay() {
+    let strings = ["Rock", "Paper", "Scissors"];
+    let play = strings[Math.floor(Math.random()*strings.length)];
+    return play;
+}
+
+function round() {
+    
+}
