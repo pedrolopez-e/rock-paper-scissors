a554e19bcb7a82c819c879017e5741d68d3bc748
Pedro López <pedrolopez.e@hotmail.com>
2022-03-04 20:26:24 -0300

    Display result in the new div with evenListener

--- script.js
+++ script.js
@@ -13,39 +13,48 @@ function playRound(e) {
         if (computerSelection.toLowerCase() === "paper") {
             //playerScore += 1;
             let result = "You win! Scissors beats Paper\n";
+            resultDisplayer.textContent = "You win! Scissors beats Paper"
             console.log(result);
         } else if (computerSelection.toLowerCase() === "rock") {
             //computerScore += 1;
             let result = "You lose! Rock beats Scissors\n";
+            resultDisplayer.textContent = "You lose! Rock beats Scissors"
             console.log(result);
         } else {
             let result = "That's a tie!\n";
+            resultDisplayer.textContent = "That's a tie!"
             console.log(result);
         }
     } else if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection.toLowerCase() === "paper") {
             //computerScore += 1;
             let result = "You lose! Paper beats rock\n";
+            resultDisplayer.textContent = "You lose! Paper beats rock"
             console.log(result);
         } else if (computerSelection.toLowerCase() === "rock") {
             let result = "That's a tie!\n";
+            resultDisplayer.textContent = "That's a tie!"
             console.log(result);
         } else {
             //playerScore += 1;
             let result = "You win! Rock beats Scissors\n";
+            resultDisplayer.textContent = "You win! Rock beats Scissors"
             console.log(result);
         }
     } else {
         if (computerSelection.toLowerCase() === "paper") {
-            let result = "That's a tie! ";
+            let result = "That's a tie!";
+            resultDisplayer.textContent = "That's a tie!"
             console.log(result);
         } else if (computerSelection.toLowerCase() === "rock") {
             //playerScore += 1;
             let result = "You win! Paper beats rock\n";
+            resultDisplayer.textContent = "You win! Paper beats rock"
             console.log(result);
         } else {
             //computerScore += 1;
             let result = "You lose! Scissors beats paper\n";
+            resultDisplayer.textContent = "You lose! Scissors beats paper"
             console.log(result);
         }
     }
@@ -55,10 +64,14 @@ function playRound(e) {
 }
 
 const buttons = document.querySelectorAll('button');
+const resultDisplayer = document.querySelector('#resultDisplayer');
 
 buttons.forEach((button) => {
     button.addEventListener('click', playRound);
 });
+
+
+
 
 /*btn.addEventListener('click', function(e) {
     console.log(e.target.id);
